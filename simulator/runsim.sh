dfca4a3e9d0a43ff8a4f9997fb0e5393fd43dde0
SpheMakh <sphemakh@gmail.com>
2014-12-08 15:15:31 +0200

    add src directory

--- simulator/runsim.sh
+++ simulator/runsim.sh
@@ -5,4 +5,4 @@ export PYTHONPATH=/code/pyxis:$PYTHONPATH
 if [ -z "$USER"]; then
   export USER=root
 fi
-pyxis CFG=/sims.cfg LOG=/results/output.log OUTFILE=/results/results OUTDIR=/results azishe
+pyxis -s ../src -s . CFG=/sims.cfg LOG=/results/output.log OUTFILE=/results/results OUTDIR=/results azishe
