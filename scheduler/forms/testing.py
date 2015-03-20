bfa474dfada6da24bc7615bb06529bb98a6ffebc
Gijs Molenaar <gijs@pythonic.nl>
2015-03-20 12:47:15 +0200

    implement

--- scheduler/forms/testing.py
+++ scheduler/forms/testing.py
@@ -1,1 +1,13 @@
-__author__ = 'gijs'
+from django.forms import CharField
+from form_utils.forms import BetterForm
+
+
+class Form(BetterForm):
+    docker_image = 'simulation/development'
+
+    class Meta:
+        fieldsets = [('global', {'fields': ['name'],
+                                 'description': 'Global settings'}),
+
+                     ]
+    name = CharField(initial='New simulation', max_length=200)
