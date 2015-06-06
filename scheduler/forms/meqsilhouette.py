9f1f7a5e498a25de3641668afa71f630925d2cf5
Tariq Blecher <blecher@jake.ru.ac.za>
2015-06-06 19:21:30 +0200

    last update

--- scheduler/forms/meqsilhouette.py
+++ scheduler/forms/meqsilhouette.py
@@ -14,6 +14,8 @@ telescope = (
         'sim_PdBI',
         'sim_Hawaii',
 	'sim_GLT',
+        'addant',
+        'addantpos',
 
 
 )
@@ -52,6 +54,7 @@ noise = (
     'sefd_PdBI',
     'sefd_Hawaii',
     'sefd_GLT',
+    'addantsefd',
 
     )
 
@@ -138,7 +141,7 @@ imaging = (
 )
 
 
-
+)
 
 class Form(BetterForm):
     docker_image = 'tariqblecher/meqsilhouette'
@@ -179,20 +182,23 @@ class Form(BetterForm):
     sim_Hawaii = BooleanField(label='Hawaii', required=False,initial=True)
     sim_GLT= BooleanField(label='GLT', required=False,initial=True,
 				help_text='Choose antennae set for observation')
- 
+    addant= BooleanField(label='Additional Antenna', required=False,initial=False,
+                                help_text='Add an additional Antenna to the array')
+    addantpos=CharField(label='Add. antenna position',initial='1.0 2.0 3.0 ',max_length=300,
+			help_text='x y z coordinates in units of metres?? Separated by a whitespace') 
     
 
 
     # sky setup
-    fitsname=FileField(label='fitsfile',initial='test.fits',required=True,
-				help_text='if blank a default image will be chosen')
+    fitsname=FileField(label='input sky fits',initial='test.fits',required=False,
+				help_text='if blank a default image will be chosen. Pixelsizes need to be in units of arcsec.')
     # Output visibilities
     exportms = BooleanField(label='Export Measurement Set', required=False,initial=True)
     exportuvfits = BooleanField(label='Export UV FITS', required=False,initial=False)
 
 
     # observation setup
-    ms_obslength = FloatField(label='obslength time', initial=0.25,
+    ms_obslength = FloatField(label='observation length', initial=0.25,
                               help_text='in hours')
     ms_tint = IntegerField(label='Integration time', initial=10,
                             help_text='in seconds')
@@ -216,6 +222,7 @@ class Form(BetterForm):
     sefd_PdBI = FloatField(label='PdBI', required=False,initial=1600)
     sefd_Hawaii = FloatField(label='Hawaii', required=False,initial=4900)
     sefd_GLT= FloatField(label='GLT', required=False,initial=7300)
+    addantsefd= FloatField(label='Additional Antenna', required=False,initial=7300)
     ERROR_MODELS =(
 		( 'SineError','SineError'),
 		('RandomPolc','RandomPolc'),
@@ -237,64 +244,64 @@ class Form(BetterForm):
 
     gjones_me_g_enable = BooleanField(label='enable per antennae gain and phase corruptions',required=False,initial=True)
     gjones_gain_error_model = ChoiceField(label='gain error model',required=False,choices=ERROR_MODELS,initial='SineError')
-    gjones_phase_error_model = ChoiceField(label='phase error model',required=False,choices=ERROR_MODELS,initial='SineError')
+    gjones_phase_error_model = ChoiceField(label='phase error model',required=False,choices=ERROR_MODELS,initial='SineError',help_text='Phase units are in degrees')
     gjones_gain_minval = FloatField(label='gain minval',required=False,initial=0.1)
     gjones_gain_maxval = FloatField(label='gain maxval',required=False,initial=4)
-    gjones_gain_min_period = FloatField(label='gain min period',required=False,initial=4)
-    gjones_gain_max_period = FloatField(label='gain max period',required=False,initial=300)
+    gjones_gain_min_period = FloatField(label='gain min period (hours)',required=False,initial=0.01)
+    gjones_gain_max_period = FloatField(label='gain max period(hours)',required=False,initial=0.5)
     gjones_phase_maxerr = FloatField(label='phase maxerr',required=False,initial=10)
-    gjones_phase_min_period = FloatField(label='phase min period',required=False,initial=10)
-    gjones_phase_max_period = FloatField(label='phase max period',required=False,initial=300)
-    gjones_gain_min0 = FloatField(label='gain min0',required=False,initial=0)
-    gjones_gain_max0 = FloatField(label='gain max0',required=False,initial=0)
-    gjones_gain_max1 = FloatField(label='gain max1',required=False,initial=0)
-    gjones_gain_max2 = FloatField(label='gain max2',required=False,initial=0)
-    gjones_gain_max3 = FloatField(label='gain max3',required=False,initial=0)
-    gjones_gain_scale = FloatField(label='gain scale',required=False,initial=0)
+    gjones_phase_min_period = FloatField(label='phase min period',required=False,initial=0.01)
+    gjones_phase_max_period = FloatField(label='phase max period',required=False,initial=0.5)
+    gjones_gain_min0 = FloatField(label='gain min0',required=False,initial=-1)
+    gjones_gain_max0 = FloatField(label='gain max0',required=False,initial=1)
+    gjones_gain_max1 = FloatField(label='gain max1',required=False,initial=2)
+    gjones_gain_max2 = FloatField(label='gain max2',required=False,initial=1)
+    gjones_gain_max3 = FloatField(label='gain max3',required=False,initial=1)
+    gjones_gain_scale = FloatField(label='gain time scale variability (hours)',required=False,initial=0.1)
     gjones_gain_offset = FloatField(label='gain offset',required=False,initial=0)
-    gjones_phase_min0 = FloatField(label='phase min0',required=False,initial=0)
-    gjones_phase_max0 = FloatField(label='phase max0',required=False,initial=0)
-    gjones_phase_max1 = FloatField(label='phase max1',required=False,initial=0)
-    gjones_phase_max2 = FloatField(label='phase max2',required=False,initial=0)
-    gjones_phase_max3 = FloatField(label='phase max3',required=False,initial=0)
-    gjones_phase_scale = FloatField(label='phase scale',required=False,initial=0)
+    gjones_phase_min0 = FloatField(label='phase min0',required=False,initial=-10)
+    gjones_phase_max0 = FloatField(label='phase max0',required=False,initial=10)
+    gjones_phase_max1 = FloatField(label='phase max1',required=False,initial=20)
+    gjones_phase_max2 = FloatField(label='phase max2',required=False,initial=10)
+    gjones_phase_max3 = FloatField(label='phase max3',required=False,initial=10)
+    gjones_phase_scale = FloatField(label='phase time scale variability (hours)',required=False,initial=0.1)
     gjones_phase_offset = FloatField(label='phase offset',required=False,initial=0)
-    gjones_gain_offset = FloatField(label='gain offset',required=False,initial=0)
-    gjones_phase_offset = FloatField(label='phase offset',required=False,initial=0)
+    gjones_gain_offset = FloatField(label='gain offset',required=False,initial=5)
+    gjones_phase_offset = FloatField(label='phase offset',required=False,initial=10)
  
     ejones_me_e_enable =  BooleanField(initial=True,label='enable',required=False)
     ejones_me_epe_enable =  BooleanField(initial=True,label='enable pointing errors',required=False,)
 
     ejones_pe_l_error_model = ChoiceField(label='pe l error model',required=False,choices=ERROR_MODELS,initial='SineError')
-    ejones_pe_m_error_model = ChoiceField(label='pe m error model',required=False,choices=ERROR_MODELS,initial='SineError')
+    ejones_pe_m_error_model = ChoiceField(label='pe m error model',required=False,choices=ERROR_MODELS,initial='SineError',help_text='Pointing errors are in units of arcseconds')
 
 
-    ejones_pe_l_maxerr = FloatField(label='pe l maxerr',required=False,initial=10)
-    ejones_pe_l_min_period = FloatField(label='pe l min period',required=False,initial=10)
-    ejones_pe_l_max_period = FloatField(label='pe l max period',required=False,initial=300)
-    ejones_pe_m_maxerr = FloatField(label='pe m maxerr',required=False,initial=10)
-    ejones_pe_m_min_period = FloatField(label='pe m min period',required=False,initial=10)
-    ejones_pe_m_max_period = FloatField(label='pe m max period',required=False,initial=300)
+    ejones_pe_l_maxerr = FloatField(label='pe l maxerr',required=False,initial=2)
+    ejones_pe_l_min_period = FloatField(label='pe l min period',required=False,initial=0.5)
+    ejones_pe_l_max_period = FloatField(label='pe l max period',required=False,initial=3)
+    ejones_pe_m_maxerr = FloatField(label='pe m maxerr',required=False,initial=2)
+    ejones_pe_m_min_period = FloatField(label='pe m min period',required=False,initial=0.5)
+    ejones_pe_m_max_period = FloatField(label='pe m max period',required=False,initial=3)
 
-    ejones_pe_l_min0 = FloatField(label='pe l min0',required=False,initial=0)
-    ejones_pe_l_max0 = FloatField(label='pe l max0',required=False,initial=0)
-    ejones_pe_l_max1 = FloatField(label='pe l max1',required=False,initial=0)
-    ejones_pe_l_max2 = FloatField(label='pe l max2',required=False,initial=0)
+    ejones_pe_l_min0 = FloatField(label='pe l min0',required=False,initial=-0.5)
+    ejones_pe_l_max0 = FloatField(label='pe l max0',required=False,initial=0.5)
+    ejones_pe_l_max1 = FloatField(label='pe l max1',required=False,initial=0.5)
+    ejones_pe_l_max2 = FloatField(label='pe l max2',required=False,initial=0.5)
     ejones_pe_l_max3 = FloatField(label='pe l max3',required=False,initial=0)
-    ejones_pe_l_offset = FloatField(label='pe l offset',required=False,initial=0)
-    ejones_pe_l_scale = FloatField(label='pe l scale',required=False,initial=0)
-    ejones_pe_m_min0 = FloatField(label='pe m min0',required=False,initial=0)
-    ejones_pe_m_max0 = FloatField(label='pe m max0',required=False,initial=0)
-    ejones_pe_m_max1 = FloatField(label='pe m max1',required=False,initial=0)
-    ejones_pe_m_max2 = FloatField(label='pe m max2',required=False,initial=0)
+    ejones_pe_l_offset = FloatField(label='pe l offset',required=False,initial=0.1)
+    ejones_pe_l_scale = FloatField(label='pe l time scale variability(hours)',required=False,initial=2)
+    ejones_pe_m_min0 = FloatField(label='pe m min0',required=False,initial=-0.5)
+    ejones_pe_m_max0 = FloatField(label='pe m max0',required=False,initial=0.5)
+    ejones_pe_m_max1 = FloatField(label='pe m max1',required=False,initial=0.5)
+    ejones_pe_m_max2 = FloatField(label='pe m max2',required=False,initial=0.5)
     ejones_pe_m_max3 = FloatField(label='pe m max3',required=False,initial=0)
-    ejones_pe_m_offset = FloatField(label='pe m offset',required=False,initial=0)
-    ejones_pe_m_scale = FloatField(label='pe m scale',required=False,initial=0)
-    ejones_pe_l_offset = FloatField(label='pe l offset',required=False,initial=0)
-    ejones_pe_m_offset = FloatField(label='pe m offset',required=False,initial=0)
+    ejones_pe_m_offset = FloatField(label='pe m offset',required=False,initial=0.1)
+    ejones_pe_m_scale = FloatField(label='pe m time scale variability',required=False,initial=2)
+    ejones_pe_l_offset = FloatField(label='pe l offset',required=False,initial=0.1)
+    ejones_pe_m_offset = FloatField(label='pe m offset',required=False,initial=0.1)
 
 #ISM scattering gaussian
-    ismscatter=BooleanField(initial=True, required=False)
+    ismscatter=BooleanField(initial=True, required=False,help_text='Ensure the pixelsize in fits header is correct')
 
     ism_major_axis=FloatField(label='major axis', initial=1.309,
 			  help_text='mas/cm^2') 
@@ -308,14 +315,14 @@ class Form(BetterForm):
     
     # imaging settings
    
-    im_npix = IntegerField(label='Image size', required=True,initial=128,help_text='in pixels (default is input image size)')
-    im_cellsize = FloatField(label='Pixel size', required=True,initial=1,help_text='in micro arcseconds (default is input pixel size)')
+    im_npix = IntegerField(label='Image size',required=True, help_text='in pixels (default is input image size)')
+    im_cellsize = FloatField(label='Pixel size',required=True, help_text='in micro arcseconds (default is input pixel size)')
     
    
-    im_uvweight=ChoiceField(label='UV weight',required=False,choices=UV_WEIGHT,initial='uniform')
-    im_robust=FloatField(label='robust',initial=0)
-    im_operation=ChoiceField(label='Clean operation',choices=OPERATION,initial='clark')
-    im_niter=FloatField(label='Number of iterations',required=False,initial=0, help_text='for a dirty image set this to zero')
+    im_uvweight=ChoiceField(Choices=UV_WEIGHT,initial=uniform,label='Pixel size')
+    im_robust= FloatField(label='Robust',initial=0)
+    im_operation=ChoiceField(label='Clean operation',Choices=OPERATION,initial=clark)
+    im_niter=FloatField(label='Number of iterations', help_text='for a dirty image set this to zero')
     im_gain=FloatField(label='Loop Gain',initial=0.1)
     im_threshold=FloatField(label='Clean Threshold', initial=0)
     im_stokes=CharField(label='STOKES',initial='I')
